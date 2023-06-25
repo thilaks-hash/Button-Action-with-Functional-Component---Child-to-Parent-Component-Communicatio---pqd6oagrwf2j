46e7585c5ce5c634c955c6e158087de1d83a8c91
thilaks-hash <thilagavathyr025@gmail.com>
2023-06-26 00:27:50 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [para, setPara] = useState(false);
+  const visiblePara = () => {
+    setPara(true);
+  };
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={visiblePara}>
+        Click
+      </button>
+      {para && (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      )}
     </div>
   );
 }
-
 
 export default App;
